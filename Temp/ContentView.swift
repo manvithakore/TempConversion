90ddf66d53e1460b2cee5e4f7972db62a2d1b836
manvithakore <manvi.thakore@gmail.com>
2021-05-16 21:30:25 -0600

    completed the app

--- Temp/ContentView.swift
+++ Temp/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    @State private var value = ""
+    
+    let inputUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    let outputUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var conversion: Double {
+        let input = Double(value) ?? 0
+        let inputU = inputUnits[inputUnit]
+        let outputU = outputUnits[outputUnit]
+        var final = input
+        
+        if inputU == outputU {
+            return final
+        }
+        else if inputU == "Celsius" && outputU == "Fahrenheit" {
+            final = ((input / 5) * 9) + 32
+        }
+        else if inputU == "Celsius" && outputU == "Kelvin" {
+            final = input + 273.15
+        }
+        else if inputU == "Fahrenheit" && outputU == "Celsius" {
+            final = ((input - 32) * 5) / 9
+        }
+        else if inputU == "Fahrenheit" && outputU == "Kelvin" {
+            final = ((input - 32) * 5) / 9 + 273.15
+        }
+        else if inputU == "Kelvin" && outputU == "Celsius" {
+            final = input - 273.15
+        }
+        else {
+            final = (((input - 273.15) / 5) * 9) + 32
+        }
+        return final;
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Input unit")) {
+                    Picker("Temperature Input Unit", selection: $inputUnit) {
+                        ForEach(0 ..< inputUnits.count) {
+                            Text("\(self.inputUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section() {
+                    TextField("Temperature", text: $value).keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Output unit")) {
+                    Picker("Temperature Output Unit", selection: $outputUnit) {
+                        ForEach(0 ..< outputUnits.count) {
+                            Text("\(self.outputUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Conversion")) {
+                    Text("\(conversion)")
+                }
+            }
+            .navigationBarTitle("Temp Conversion")
+        }
     }
 }
 
