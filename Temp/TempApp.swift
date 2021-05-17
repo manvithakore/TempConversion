42f9148e294ca4151e7b88523a21b516572c2b10
manvithakore <manvi.thakore@gmail.com>
2021-05-16 20:00:02 -0600

    Initial Commit

---
+++ Temp/TempApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempApp.swift
+//  Temp
+//
+//  Created by Manvi Thakore on 2021-05-16.
+//
+
+import SwiftUI
+
+@main
+struct TempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
